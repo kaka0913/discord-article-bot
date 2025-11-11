1681fa95d7fddb1cf4465c338d8fc7f560fd4cd4
Yuichiro Kabutan <131236119+kaka0913@users.noreply.github.com>
2025-11-12 00:42:13 +0900

    いらん
    Removed comments regarding automatic index creation and composite indexes.

--- terraform/modules/firestore/main.tf
+++ terraform/modules/firestore/main.tf
@@ -11,7 +11,3 @@ resource "google_firestore_database" "database" {
   # ABANDON: terraform destroyでもFirestoreは削除されず、GCPコンソールから手動削除が必要
   deletion_policy = "ABANDON"
 }
-
-# 単一フィールドインデックスは自動的に作成されるため、
-# 明示的なインデックス定義は不要です
-# 複合インデックスが必要になった場合のみ、ここに追加してください
